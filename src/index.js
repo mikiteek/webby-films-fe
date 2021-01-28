16add494584759134c3703b4dfa577df7693a301
mikiteek <mikiteek@gmail.com>
2021-01-28 18:48:57 +0200

    Header, Navigation component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
+
 import App from "./App";
+import "./static/scss/main.scss";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+       <App />
+     </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
